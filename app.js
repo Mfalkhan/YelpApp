e7ed732e3bcd2db5befc91b7d24805d9d96f11eb
Mfalkhan <=>
2019-09-23 05:32:45 +0530

    v2 c3

--- app.js
+++ app.js
@@ -2,15 +2,39 @@ var express = require("express");
 var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_app", {
+ useNewUrlParser: true
+}).then(() => {
+ console.log('Connected to MongoDB');
+}).catch(err => {
+ console.log(err.message);
+});
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-var campgrounds = [
-  {name:"Salmon Scales", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-  {name:"Textile Hill", image:"https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-  {name:"Rattler's Sand", image:"https://images.unsplash.com/photo-1496545672447-f699b503d270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"},
-  {name:"Rattler's Sand", image:"https://images.unsplash.com/photo-1496545672447-f699b503d270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"},
-]
+// Campgroung Schema or Blueprint
+const CampgroundSchema = new mongoose.Schema({
+  name:String,
+  image:String
+});
+
+// Creating a Model
+const Campground = mongoose.model("Campground",CampgroundSchema);
+
+// Campground.create(
+//   {
+//     name:"Textile Hill",
+//     image:"https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+//   },(err,campground) => {
+//         if(err){
+//           console.log(err);
+//         }else{
+//           console.log(campground);
+//         }
+//   });
+
 
 app.set("view engine","ejs");
 
@@ -20,15 +44,27 @@ app.get("/",function(req,res){
 
 app.get("/campgrounds",function(req,res){
 
-  res.render("campgrounds",{campgrounds})
+  Campground.find({},(err,campgrounds) => {
+        if(err){
+          console.log(err);
+        }else{
+          res.render("campgrounds",{campgrounds})
+        }
+  })  
 })
 
 app.post("/campgrounds",function(req,res){
    var name = req.body.name;
    var image = req.body.image;
    var newCampground = {name:name,image:image};
-   campgrounds.push(newCampground);
-   res.redirect("/campgrounds");
+   
+   Campground.create(newCampground,(err,campground) => {
+      if(err){
+        console.log(err);
+      }else{
+        res.redirect("/campgrounds");
+      }
+   })
 })
 
 app.get("/campgrounds/new",function(req,res){
