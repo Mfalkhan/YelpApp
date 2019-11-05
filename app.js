5174a83b50a912b5eee1e1f7ed518ca5e21277ea
Mfalkhan <=>
2019-11-05 22:45:27 +0530

    c4

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+const Campground = require("./models/campgrounds");
+const seedDB = require("./seeds");
+const Comment = require("./models/comments");
 
+// avoiding the mongo DB error
 mongoose.connect("mongodb://localhost/yelp_app", {
  useNewUrlParser: true
 }).then(() => {
@@ -14,19 +18,13 @@ mongoose.connect("mongodb://localhost/yelp_app", {
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-// Campgroung Schema or Blueprint
-const CampgroundSchema = new mongoose.Schema({
-  name:String,
-  image:String
-});
 
-// Creating a Model
-const Campground = mongoose.model("Campground",CampgroundSchema);
 
 // Campground.create(
 //   {
 //     name:"Textile Hill",
-//     image:"https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+//     image:"https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+//     description:"A simple yet beautiful hill"
 //   },(err,campground) => {
 //         if(err){
 //           console.log(err);
@@ -38,9 +36,14 @@ const Campground = mongoose.model("Campground",CampgroundSchema);
 
 app.set("view engine","ejs");
 
+// Root Route
 app.get("/",function(req,res){
   res.render("landing");
 })
+
+seedDB();
+
+// INDEX ROUTE displays all the campgrounds
 
 app.get("/campgrounds",function(req,res){
 
@@ -48,15 +51,18 @@ app.get("/campgrounds",function(req,res){
         if(err){
           console.log(err);
         }else{
-          res.render("campgrounds",{campgrounds})
+          res.render("campgrounds/index",{campgrounds})
         }
   })  
 })
 
+// CREATE ROUTE adds a new campground to the database
+
 app.post("/campgrounds",function(req,res){
    var name = req.body.name;
    var image = req.body.image;
-   var newCampground = {name:name,image:image};
+   var description = req.body.description;
+   var newCampground = {name:name,image:image,description:description};
    
    Campground.create(newCampground,(err,campground) => {
       if(err){
@@ -67,11 +73,62 @@ app.post("/campgrounds",function(req,res){
    })
 })
 
+// NEW ROUTE displays form to enter new campground
+
 app.get("/campgrounds/new",function(req,res){
-   res.render("new");
+   res.render("campgrounds/new");
+})
+
+// SHOW ROUTE 
+
+app.get("/campgrounds/:id",function(req,res){
+  // find the campground with the provided id
+  Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground){
+    if(err){
+      console.log(err);
+    }else{
+      // render show template
+      res.render("campgrounds/show",{campground:foundCampground});
+    }
+  })
 })
 
 
+// COMMENT ROUTES
+
+app.get("/campgrounds/:id/comments/new",(req,res) =>{
+
+      //  Find the campground 
+      Campground.findById(req.params.id,(err,campground) =>{
+          if(err){
+            console.log(err);
+          }else{
+            res.render("comments/new",{campground})
+          }
+      });
+});
+
+app.post("/campgrounds/:id/comments",(req,res) =>{
+
+    // Find the campground
+    Campground.findById(req.params.id,(err,campground) =>{
+
+        if(err){
+          console.log(err);
+          res.redirect("/campgrounds");
+        }else{
+          Comment.create(req.body.comment,(err,comment) =>{
+              if(err){
+                console.log(err);
+              }else{
+                campground.comments.push(comment);
+                campground.save();
+                res.redirect("/campgrounds/" + campground._id);
+              }
+          });
+        }
+    });
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port,function(){
---
+++ seeds.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Campground = require("./models/campgrounds");
+const Comment = require("./models/comments");
+
+
+const data = [
+        {
+            name :"simple tree in the woods",
+            image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.XWXdq9R-W9Zik2vRW154vQHaE8%26pid%3DApi&f=1",
+            description:"A simple yet beautiful camp"
+        },
+        {
+            name :"clouds rest",
+            image:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.goodsam.com%2Ftrailerlifedirectory%2Flargefeatured%2F1000x%2Fpho_200000698_04.jpg&f=1&nofb=1",
+            description:"A simple cloud camp"
+        },
+        {
+            name :"simple tree in the woods",
+            image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.yb76EXfcGxbb3jlNzH2vBgHaEK%26pid%3DApi&f=1",
+            description:"A simple yet beautiful camp"
+        },
+        {
+            name :" lone tree in the woods",
+            image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.ss1kiynZ0EzlxOyBtISy5gHaE0%26pid%3DApi&f=1",
+            description:"A simple yet beautiful camp"
+        }
+
+];
+
+function seedDB(){
+
+        // remove all the campgrounds
+        Campground.remove({},err =>{
+            if(err){
+                console.log(err);
+            }
+            console.log("campground removed");
+
+            data.forEach(seed=>{
+                Campground.create(seed,(err,campground)=>{
+                        if(err){
+                            console.log(err);
+                        }else{
+                            console.log("added a campground");
+                        }
+                        // create a comment
+
+                        Comment.create({
+
+                                text :"not a good thing",
+                                author:"farhan khan"
+
+                        },(err,comment)=>{
+                            campground.comments.push(comment);
+                            campground.save();
+                            console.log("created new comment");
+                        });
+                });
+            });
+        });
+    }
+
+    module.exports = seedDB;
